4a6f841ed35dafdf67774b7d6754d46b3119467d
nik-rawat <nr2192004@gmail.com>
2024-12-07 22:44:48 +0530

    changes done

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,15 +6,6 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "chatkaro-5b466.firebaseapp.com",
-  projectId: "chatkaro-5b466",
-  storageBucket: "chatkaro-5b466.appspot.com",
-  messagingSenderId: "1081210402428",
-  appId: "1:1081210402428:web:b832efdd918e57ebddbbf9"
-};
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
